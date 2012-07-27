5176c46d55b61a1bec069940ca38b8637407c270
Anthony Ferrara <ircmaxell@gmail.com>
2012-07-27 11:23:03 -0400

    Update encryption to use CFB, hashing and PKCS7 padding

--- Shield/Session.php
+++ Shield/Session.php
@@ -16,11 +16,25 @@ class Session extends Base
      */
     private $_savePath      = '';
 
+	/**
+	 * The MCrypt Cipher To Use For Encrypting Data
+	 * @var string
+	 */
+	private $_cipher = MCRYPT_RIJNDAEL_256;
+
     /**
-     * Salt for hashing the session data
+     * Key for storing the session data
+	 * This needs to be a strong cryptographic key
      * @var string
      */
-    private $_salt          = 't3st!ng';
+    private $_encKey        = '';
+
+	/**
+	 * Key for signing the encrypted session data
+	 * This needs to be a strong cryptographic key
+	 * @var string
+	 */
+	private $_sigKey        = '';
 
     /**
      * Init the object, set up the session config handling
@@ -41,6 +55,17 @@ class Session extends Base
         parent::__construct($di);
     }
 
+	/**
+	 * Overwrite the key prior to destruction
+	 *
+	 * @return null
+	 */
+	public function __destruct() {
+		$this->_sigKey &= str_repeat(chr(0), strlen($this->_sigKey));
+		$this->_encKey &= str_repeat(chr(0), strlen($this->_encKey));
+	}
+
+
     /**
      * Write to the session
      * 
@@ -51,22 +76,43 @@ class Session extends Base
      */
     public function write($id,$data)
     {
+		$this->checkKeys();
         $path = $this->_savePathRoot.'/shield_'.$id;
-        $data = mcrypt_encrypt(MCRYPT_RIJNDAEL_256, $this->_salt, $data, MCRYPT_MODE_ECB);
+
+		$ivSize = mcrypt_get_iv_size($this->_cipher, MCRYPT_MODE_CFB);
+		$iv = mcrypt_create_iv($ivSize, MCRYPT_DEV_URANDOM);
+
+		$data = $this->pad($data);
+
+        $cipherText = mcrypt_encrypt($this->_cipher, $this->_encKey, $data, MCRYPT_MODE_CFB, $iv);
+		$hash = hash_hmac('sha512', $iv . $cipherText, $this->_sigKey, true);
+		$data = $iv . $cipherText . $hash;
 
         file_put_contents($path,$data);
     }
 
     /**
-     * Set the salt for the sesion encryption to use (default is set)
+	 * Set the key for encrypting the data with
      * 
-     * @param string $salt Salt string
+     * @param string $key Key string
      * 
      * @return null
      */
-    public function setSalt($salt)
+    public function setEncryptionKey($key)
     {
-        $this->_salt = $salt;
+        $this->_encKey = $key;
+    }
+
+	/**
+	 * Set the key for signing the data with
+     * 
+     * @param string $key Key string
+     * 
+     * @return null
+     */
+    public function setSignatureKey($key)
+    {
+        $this->_sigKey = $key;
     }
 
     /**
@@ -78,12 +124,29 @@ class Session extends Base
      */
     public function read($id)
     {
+		$this->checkKeys();
         $path = $this->_savePathRoot.'/shield_'.$id;
-        $data = null;
 
         if (is_file($path)) {
             $data = file_get_contents($path);
-            $data = mcrypt_encrypt(MCRYPT_RIJNDAEL_256, $this->_salt, $data, MCRYPT_MODE_ECB);
+			$ivSize = mcrypt_get_iv_size($this->_cipher, MCRYPT_MODE_CFB);
+			if (strlen($data) < $ivSize + 64 + 1) {
+				return null;
+			}
+			$iv = substr($data, 0, $ivSize);
+			$hash = substr($data, -64);
+			$cipherText = substr($data, $ivSize, -64);
+
+			// verify the hash first
+			if ($hash !== hash_hmac('sha512', $iv . $cipherText, $this->_sigKey, true)) {
+				return null;
+			}
+
+            $data = mcrypt_decrypt($this->_cipher, $this->_salt, $encKey, MCRYPT_MODE_CFB, $iv);
+			$data = $this->stripPadding($data);
+			if (!$data) {
+				return null;
+			}
         }
 
         return $data;
@@ -161,5 +224,43 @@ class Session extends Base
         session_start();
         $_SESSION = $sess;
     }
+
+	/**
+	 * Check the keys to determine if they were setup properly
+	 *
+	 * @return null
+	 * @throws RuntimeException when either key is invalid
+	 */
+	private function checkKeys() {
+		$valid = true;
+		if (strlen($this->_encKey) !== mcrypt_get_key_size($this->_cipher)) {
+			$valid = false;
+		}
+		if (strlen($this->_sigKey) < 64) {
+			$valid = false;
+		}
+		if (!$valid) {
+			throw new \RuntimeException("Keys were not setup properly prior to session start");
+		}
+	}
+
+	private function pad($data) {
+		$blockSize = mcrypt_get_block_size($this->_cipher, MCRYPT_MODE_CFB);
+		$padSize = $blockSize - (strlen($data) % $blockSize);
+		return $data . str_repeat(chr($padSize), $padSize);
+	}
+
+	private function stripPadding($data) {
+		$padSize = ord($data[strlen($data) - 1]);
+		if ($padSize > strlen($data)) {
+			return false;
+		}
+		$padBlock = str_repeat(chr($padSize), $padSize);
+		if (substr($data, -1 * $padSize) !== $padSize) {
+			// Invalid padding detected!
+			return false;
+		}
+		return substr($data, 0, -1 * $padSize);
+	}
 
 }
